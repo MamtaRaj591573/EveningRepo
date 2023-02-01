4ca4932ecf3517c1c29f38fdde43e94aa2c2d3ad
MamtaRaj591573 <45228546+MamtaRaj591573@users.noreply.github.com>
2023-02-01 20:40:18 +0530

    Update TestNGDemo1.java

--- VTiger/src/test/java/practice/TestNGDemo1.java
+++ VTiger/src/test/java/practice/TestNGDemo1.java
@@ -7,12 +7,12 @@ public class TestNGDemo1
 	@Test
 	public void m1()
 	{
-		System.out.println("m1 running");
+		System.out.println("m1 running modified from github");
 	}
 	@Test
 	public void m2()
 	{
-		System.out.println("m2 running");
+		System.out.println("m2 running modified from github");
 	}
 
 }
